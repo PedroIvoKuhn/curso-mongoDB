c9af4aa76d94e1f971a08413b3a7eec359c042a7
Pedro Ivo Kuhn <kuhnpedroivo@gmail.com>
2023-08-29 20:02:35 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/pedro/cursomongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pedro.cursomongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
