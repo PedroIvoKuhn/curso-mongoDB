e50de605bf20a3a2546db2cf15784d7c664c9381
Pedro Ivo Kuhn <kuhnpedroivo@gmail.com>
2023-10-02 15:08:23 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/pedro/cursomongodb/resources/UserResource.java
+++ src/main/java/com/pedro/cursomongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.pedro.cursomongodb.domain.Post;
 import com.pedro.cursomongodb.domain.User;
 import com.pedro.cursomongodb.dto.UserDTO;
 import com.pedro.cursomongodb.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+     @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
